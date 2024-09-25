bf3dce4cc011d8f8a445bb911e6d583962305fa9
AryaKopet <aryasokker23@gmail.com>
2024-09-25 13:35:49 +0700

    Membuat API Perhitungan

--- app/Http/Controllers/Api/SegitigaController.php
+++ app/Http/Controllers/Api/SegitigaController.php
@@ -6,14 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 
-class SegitigaController extends Controller {
-    public function hitungSegitiga (Request $request)
+class SegitigaController extends Controller { 
+    //Menghitung Luas Segitiga
+    public function luasSegitiga (Request $request)
     {
         $alas = $request -> alas;
         $tinggi = $request -> tinggi;
         $luas = 0.5 * $alas * $tinggi;
-        return new PostResource(true, 'Berhasil hitung luas segitiga', [
+        return new PostResource(true, 'Berhasil menghitung luas segitiga', [
             'hasil' => $luas
+        ]);
+    }
+
+    //Menghitung Keliling Segitiga
+    public function kelilingSegitiga (Request $request)
+    {
+        $sisi = $request -> sisi;
+        $keliling = $sisi + $sisi + $sisi;
+        return new PostResource(true, 'Berhasil menghitung keliling segitiga', [
+            'hasil' => $keliling
         ]);
     }
 }
