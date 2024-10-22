402c8fedabf7d2966c5f0fa007bf90b240b334e2
AryaKopet <aryasokker23@gmail.com>
2024-10-22 12:19:54 +0700

    menambahkan api pendaftaran-mahasiswa untuk alpine js praktikum

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
         return $request->user();
 })->middleware(Authenticate::using('sanctum'));
-
-//posts
 Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+Route::apiResource('/pendaftaran-mahasiswa', App\Http\Controllers\Api\PendaftaranMahasiswaController::class);
 Route::post('/persegi', [App\Http\Controllers\Api\PersegiController::class,
         'hitungPersegi']);
 Route::post('/luasSegitiga', [App\Http\Controllers\Api\SegitigaController::class,
