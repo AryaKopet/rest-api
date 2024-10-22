7a2c1925077f09ccb0e25df03603ca96f237a2e3
AryaKopet <aryasokker23@gmail.com>
2024-10-22 13:07:15 +0700

    edit PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -174,16 +174,12 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-
         //find post by ID
         $post = Post::find($id);
-
         //delete image
         Storage::delete('public/posts/'.basename($post->image));
-
         //delete post
         $post->delete();
-
         //return response
         return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
     }
